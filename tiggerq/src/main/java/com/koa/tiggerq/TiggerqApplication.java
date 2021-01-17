1f3abb78f669ab6fb7550d43c8ab13e76215349b
Joe Wang <zhouqiang.wang@hotmail.com>
2021-01-17 11:43:58 -0800

    Add TigerQ Springboot codes.

---
+++ tiggerq/src/main/java/com/koa/tiggerq/TiggerqApplication.java
@@ -0,0 +1,13 @@
+package com.koa.tiggerq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiggerqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiggerqApplication.class, args);
+	}
+
+}
---
+++ tiggerq/src/test/java/com/koa/tiggerq/TiggerqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koa.tiggerq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiggerqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
