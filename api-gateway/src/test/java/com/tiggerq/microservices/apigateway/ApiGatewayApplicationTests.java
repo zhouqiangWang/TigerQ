28bbfe147cc6449ae5959e6b3eacf0b7c7e86456
Joe Wang <zhouqiang.wang@hotmail.com>
2021-01-24 20:44:06 -0800

    add api-gateway

---
+++ api-gateway/src/test/java/com/tiggerq/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiggerq.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
