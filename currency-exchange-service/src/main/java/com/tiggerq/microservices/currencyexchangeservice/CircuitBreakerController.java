5da6c9ac443ea7dda80dc66018d269c125f8828d
Joe Wang <zhouqiang.wang@hotmail.com>
2021-01-30 11:25:01 -0800

    add circuitbreaker config

---
+++ currency-exchange-service/src/main/java/com/tiggerq/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,32 @@
+package com.tiggerq.microservices.currencyexchangeservice;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+    private Logger logger =
+            LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+//    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    public String sampleApi() {
+        logger.info("sample API received.");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080//dummy",
+                String.class);
+
+        return forEntity.getBody();
+    }
+
+    public String hardcodedResponse(Throwable exception) {
+
+        return "fallback-response";
+    }
+}
